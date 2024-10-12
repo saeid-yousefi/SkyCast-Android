cc0741160324b56791451939bac772cc3443842a
saeid-yousefi <saeid.yousefi.1373@gmail.com>
2024-10-12 17:49:26 +0330

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.myket.ir")
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.myket.ir")
+        }
+    }
+}
+
+
+rootProject.name = "SkyCast"
+include(":app")
