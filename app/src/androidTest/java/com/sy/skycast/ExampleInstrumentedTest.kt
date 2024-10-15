e63f951b8b39b0455d64a108c2e4b2a82a8f944d
saeid-yousefi <saeid.yousefi.1373@gmail.com>
2024-10-15 16:01:40 +0330

    adding build logic

--- app/src/androidTest/java/com/sy/skycast/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sy/skycast/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.sy.filmora", appContext.packageName)
+        assertEquals("com.sy.skycast", appContext.packageName)
     }
 }
