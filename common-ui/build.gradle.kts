48a25f7509e48afc2ed68c0e2a659238f8ad00e1
saeid-yousefi <saeid.yousefi.1373@gmail.com>
2024-10-21 17:32:54 +0330

    adding navigation

--- common-ui/build.gradle.kts
+++ common-ui/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.skycast.android.library)
     alias(libs.plugins.skycast.compose.library)
+    alias(libs.plugins.skycast.test.unit)
+    alias(libs.plugins.skycast.test.android)
 }
 
 android {
