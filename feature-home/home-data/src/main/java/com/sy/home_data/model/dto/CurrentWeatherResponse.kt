54fbc7b5dadf86ed8d1d88a9faed6f7cce7ca100
saeid-yousefi <saeid.yousefi.1373@gmail.com>
2024-12-08 18:32:52 +0330

    update icons

--- feature-home/home-data/src/main/java/com/sy/home_data/model/dto/CurrentWeatherResponse.kt
+++ feature-home/home-data/src/main/java/com/sy/home_data/model/dto/CurrentWeatherResponse.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CurrentWeatherResponse(
+    val dt: Double? = null,
     val weather: List<WeatherResponse>,
     val main: MainResponse,
     val wind: WindResponse
